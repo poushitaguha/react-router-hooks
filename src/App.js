2dcdce6766562afdc4b2eab39271200ce51f3256
poushitaguha <pguha.kolkata@gmail.com>
2020-02-19 15:47:49 -0500

    Use router hook useLocation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Fragment } from 'react';
 import './index.css';
 
-import { BrowserRouter as Router, Route, Link, Switch, Redirect, useHistory, useParams } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+  useHistory,
+  useParams,
+  useLocation
+} from 'react-router-dom';
 
 export default function App() {
   const name = "Poushita Guha";
-  const isAuthenticated = true;
 
   return (
     <Router>
@@ -19,15 +27,9 @@ export default function App() {
         </nav>
         <Switch>
           <Route exact path="/" component={Home} />
-          {
-            isAuthenticated ?
-              <>
-                <Route path="/about/:name" component={About} />
-                <Route path="/contact" component={Contact} />
-              </>
-              :
-              <Redirect to="/" />
-          }
+          <Route path="/about/:name" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route render={() => <h1>404: page not found</h1>} />
         </Switch>
       </main>
     </Router>
@@ -57,12 +59,14 @@ const About = () => {
 
 // Contact Page 
 const Contact = () => {
+  const { pathname } = useLocation();
   const history = useHistory();
   return (
     //props.history.push
     <Fragment>
       <h1>Contact</h1>
       <button onClick={() => history.push('/')} >Go to Home</button>
+      <p>Current URL: http://localhost:3000{pathname}</p>
       <FakeText />
     </Fragment>
   )
