acebc41a9e884cec2966e738267df356927ff855
poushitaguha <pguha.kolkata@gmail.com>
2020-02-19 13:49:14 -0500

    Rendering routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Fragment } from 'react';
 import './index.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 export default function App() {
   return (
-    <main>
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </nav>
-    </main>
+    <Router>
+      <main>
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </nav>
+        <Route path="/" render={() => <h1>Welcome!!</h1>} />
+      </main>
+    </Router>
   );
 }
 
